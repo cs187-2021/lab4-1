e05ac61cf059613f0158943fcdff6702a221deba
cs187staff <dengyuntian@g.harvard.edu>
2021-11-09 18:29:47 +0000

    automatic update

--- tests/derivation_5.py
+++ tests/derivation_5.py
@@ -1,6 +1,6 @@
 test = {   'name': 'derivation_5',
     'points': 1,
-    'suites': [   {   'cases': [{'code': ">>> isinstance(NP__flights_from_boston_to_new_york('LY01'), bool)\nTrue", 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> isinstance(NP__flights_from_boston_to_new_york(LY01), bool)\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
--- tests/dl10_endpoints.py
+++ tests/dl10_endpoints.py
@@ -1,3 +1,3 @@
 test = {   'name': 'dl10_endpoints',
     'points': 1,
-    'suites': [{'cases': [{'code': '>>> isinstance(result, bool)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> isinstance(prop1, bool)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
